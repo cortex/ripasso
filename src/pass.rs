e39f47c15c1701473b570f39b0f11fddc4698a75
Joakim Lundborg <joakim.lundborg@gmail.com>
2018-01-31 23:01:21 +0100

    Fix clippy suggestions

--- src/pass.rs
+++ src/pass.rs
@@ -29,16 +29,13 @@ impl PasswordEntry {
         // Decrypt password
         let mut ctx = gpgme::Context::from_protocol(gpgme::Protocol::OpenPgp).unwrap();
         let mut output = Vec::new();
-        match ctx.decrypt(&mut input, &mut output) {
-            Err(_) => {
-                println!("decryption failed");
-                return None;
-            }
-            Ok(_) => (),
+        if let Err(e) = ctx.decrypt(&mut input, &mut output) {
+            println!("decryption failed {:?}", e);
+            return None;
         }
         let password = str::from_utf8(&output).unwrap();
-        let firstline: String = password.split("\n").take(1).collect();
-        return Some(firstline);
+        let firstline: String = password.split('\n').take(1).collect();
+        Some(firstline)
     }
 }
 
@@ -48,15 +45,15 @@ pub enum PasswordEvent {
 
 pub type PasswordList = Arc<Mutex<Vec<PasswordEntry>>>;
 
-pub fn search(l: &PasswordList, query: String) -> Vec<PasswordEntry> {
+pub fn search(l: &PasswordList, query: &str) -> Vec<PasswordEntry> {
     let passwords = l.lock().unwrap();
-    fn normalized(s: &String) -> String {
+    fn normalized(s: &str) -> String {
         s.to_lowercase()
     };
-    fn matches(s: &String, q: &String) -> bool {
-        normalized(&s).as_str().contains(normalized(&q).as_str())
+    fn matches(s: &str, q: &str) -> bool {
+        normalized(s).as_str().contains(normalized(q).as_str())
     };
-    let matching = passwords.iter().filter(|p| matches(&p.name, &query));
+    let matching = passwords.iter().filter(|p| matches(&p.name, query));
     matching.cloned().collect()
 }
 
@@ -69,8 +66,8 @@ pub fn watch() -> Result<(Receiver<PasswordEvent>, PasswordList), Box<Error>> {
 
     // Spawn watcher threads
     thread::spawn(move || {
-        load_passwords(&dir, &password_tx);
-        watch_passwords(&dir, password_tx);
+        load_passwords(&dir, &password_tx).expect("failed loading passwords");
+        watch_passwords(&dir, &password_tx).expect("failed watching password directory");
     });
 
     let passwords = Arc::new(Mutex::new(Vec::new()));
@@ -78,16 +75,10 @@ pub fn watch() -> Result<(Receiver<PasswordEvent>, PasswordList), Box<Error>> {
 
     // Spawn password list update thread
     thread::spawn(move || loop {
-        match password_rx.recv() {
-            Ok(p) => {
-                let mut passwords = passwords.lock().unwrap();
-                passwords.push(p);
-                event_tx.send(PasswordEvent::NewPassword);
-            }
-            Err(e) => {
-                panic!("password receiver channel failed: {:?}", e);
-            }
-        }
+        let p = password_rx.recv().expect("password receiver channel failed");
+        let mut passwords = passwords.lock().unwrap();
+        passwords.push(p);
+        event_tx.send(PasswordEvent::NewPassword).expect("password send channel failed")
     });
     Ok((event_rx, passwords_out))
 }
@@ -101,9 +92,9 @@ fn to_name(base: &PathBuf, path: &PathBuf) -> String {
         .to_string()
 }
 
-fn to_password(base: &PathBuf, path: PathBuf) -> PasswordEntry {
+fn to_password(base: &PathBuf, path: &PathBuf) -> PasswordEntry {
     PasswordEntry {
-        name: to_name(base, &path),
+        name: to_name(base, path),
         filename: path.to_string_lossy().into_owned().clone(),
         meta: "".to_string(),
     }
@@ -125,38 +116,37 @@ fn password_dir() -> Result<PathBuf, Box<Error>> {
     if !Path::new(&pass_home).exists() {
         return Err(From::from("failed to locate password directory"));
     }
-    return Ok(Path::new(&pass_home).to_path_buf());
+    Ok(Path::new(&pass_home).to_path_buf())
 }
 
 fn load_passwords(dir: &PathBuf, tx: &Sender<PasswordEntry>) -> Result<(), SendError<PasswordEntry>> {
     let password_path_glob = dir.join("**/*.gpg");
 
     // Find all passwords
-    let ref passpath_str = password_path_glob.to_string_lossy();
+    let passpath_str = &password_path_glob.to_string_lossy();
     println!("path: {}", passpath_str);
     for entry in glob::glob(passpath_str).expect("Failed to read glob pattern") {
         match entry {
-            Ok(path) => try!(tx.send(to_password(dir, path))),
+            Ok(path) => try!(tx.send(to_password(dir, &path))),
             Err(e) => println!("{:?}", e),
         }
     }
     Ok(())
 }
 
-fn watch_passwords(dir: &PathBuf, password_tx: Sender<PasswordEntry>) -> Result<(), Box<Error>> {
+fn watch_passwords(dir: &PathBuf, password_tx: &Sender<PasswordEntry>) -> Result<(), Box<Error>> {
     let (tx, rx) = channel();
     let mut watcher: notify::RecommendedWatcher = try!(notify::Watcher::new(tx, Duration::from_secs(2)));
     try!(watcher.watch(dir, notify::RecursiveMode::Recursive));
 
     loop {
-        match rx.recv() {
+        match rx.recv(){
             Ok(event) => {
-                match event {
-                    notify::DebouncedEvent::Create(path) => try!(password_tx.send(to_password(dir, path))),
-                    _ => (),
+                if let notify::DebouncedEvent::Create(path) = event {
+                    try!(password_tx.send(to_password(dir, &path)));   
                 }
             }
-            Err(e) => println!("watch error: {:?}", e),
+            Err(e) => println!("watch error: {:?}", e)
         }
     }
 }
