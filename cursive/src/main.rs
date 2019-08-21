c3a4518503734c06b1c9a9753d549bfbbe9c1ffa
Alexander Kjäll <alexander.kjall@gmail.com>
2019-08-21 14:30:09 +0200

    Added Alexander Kjäll to the list of developers

--- cursive/src/main.rs
+++ cursive/src/main.rs
@@ -1,5 +1,5 @@
 /*  Ripasso - a simple password manager
-    Copyright (C) 2018 Joakim Lundborg
+    Copyright (C) 2019 Joakim Lundborg, Alexander Kjäll
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
