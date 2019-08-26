4aad4de8b0a46f2c65e9912f9f267da55068f6c3
Alexander Kjäll <alexander.kjall@gmail.com>
2019-08-26 18:16:29 +0200

    fix unittest by unsetting the PASSWORD_STORE_DIR env variable

--- src/pass/test.rs
+++ src/pass/test.rs
@@ -20,6 +20,7 @@ fn get_password_dir() {
 fn get_password_dir_no_env() {
     let dir = tempfile::tempdir().unwrap();
     env::set_var("HOME", dir.path());
+    env::remove_var("PASSWORD_STORE_DIR");
 
     let path = password_dir();
 
