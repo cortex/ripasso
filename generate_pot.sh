5e38f0e8413abd58330323e407b26c40065d8310
aevin <eivindsy@pvv.org>
2019-12-27 19:25:01 +0100

    update generate_pot.sh to include the new nn translation

--- generate_pot.sh
+++ generate_pot.sh
@@ -4,3 +4,4 @@ xgettext cursive/src/*rs -kgettext --sort-output -o cursive/res/ripasso-cursive.
 
 msgmerge --update cursive/res/sv.po cursive/res/ripasso-cursive.pot
 msgmerge --update cursive/res/nb.po cursive/res/ripasso-cursive.pot
+msgmerge --update cursive/res/nn.po cursive/res/ripasso-cursive.pot
