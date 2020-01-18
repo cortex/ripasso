91d8a483146808cd3b91eb39beb6386a41a4ac3e
Alexander Kjäll <alexander.kjall@gmail.com>
2020-01-18 19:35:56 +0100

    added fr to generate_pot.sh

--- generate_pot.sh
+++ generate_pot.sh
@@ -5,3 +5,4 @@ xgettext cursive/src/*rs -kgettext --sort-output -o cursive/res/ripasso-cursive.
 msgmerge --update cursive/res/sv.po cursive/res/ripasso-cursive.pot
 msgmerge --update cursive/res/nb.po cursive/res/ripasso-cursive.pot
 msgmerge --update cursive/res/nn.po cursive/res/ripasso-cursive.pot
+msgmerge --update cursive/res/fr.po cursive/res/ripasso-cursive.pot
