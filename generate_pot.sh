317932bf5a62e549f1446a31ff4db4540bbb4826
Alexander Kjäll <alexander.kjall@gmail.com>
2020-02-11 18:12:49 +0100

    fixed the german translation into the same layout as the others

--- generate_pot.sh
+++ generate_pot.sh
@@ -7,3 +7,4 @@ msgmerge --update cursive/res/nb.po cursive/res/ripasso-cursive.pot
 msgmerge --update cursive/res/nn.po cursive/res/ripasso-cursive.pot
 msgmerge --update cursive/res/fr.po cursive/res/ripasso-cursive.pot
 msgmerge --update cursive/res/it.po cursive/res/ripasso-cursive.pot
+msgmerge --update cursive/res/de.po cursive/res/ripasso-cursive.pot
